4672596e96fbe1bc6756dd816c3640100ca5d1b6
David Lastrucci <david.lastrucci@gmail.com>
2022-07-28 21:34:31 +0200

    Nested Exceptions

--- Trysil/Trysil.Exceptions.pas
+++ Trysil/Trysil.Exceptions.pas
@@ -20,8 +20,66 @@ type
 
 { ETException }
 
-  ETException = class(Exception);
+  ETException = class(Exception)
+  strict private
+    FNestedException: Exception;
+  public
+    constructor CreateFmt(
+      const AMessage: String;
+      const AArgs: array of const); overload;
+
+    constructor CreateFmt(
+      const AMessage: String;
+      const AArgs: array of const;
+      const ANestedException: Exception); overload;
+
+    constructor Create(const AMessage: String); overload;
+
+    constructor Create(
+      const AMessage: String;
+      const ANestedException: Exception); overload;
+
+    destructor Destroy; override;
+
+    property NestedException: Exception read FNestedException;
+  end;
 
 implementation
+
+{ ETException }
+
+constructor ETException.CreateFmt(
+  const AMessage: String; const AArgs: array of const);
+begin
+  CreateFmt(AMessage, AArgs, nil);
+end;
+
+constructor ETException.CreateFmt(
+  const AMessage: String;
+  const AArgs: array of const;
+  const ANestedException: Exception);
+begin
+  Create(Format(AMessage, AArgs), ANestedException);
+end;
+
+constructor ETException.Create(const AMessage: String);
+begin
+	Create(AMessage, nil);
+end;
+
+constructor ETException.Create(
+  const AMessage: String; const ANestedException: Exception);
+begin
+  inherited Create(AMessage);
+  if Assigned(ANestedException) then
+  	FNestedException := Exception(AcquireExceptionObject);
+end;
+
+destructor ETException.Destroy;
+begin
+  if Assigned(FNestedException) then
+    FNestedException.Free;
+  inherited Destroy;
+end;
 
 end.
