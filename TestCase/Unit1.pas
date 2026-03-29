043a2563d89194363748740d38059c19da1ba474
Diego Farisato <diego.farisato@gmail.com>
2026-03-29 12:20:44 -0300

    Add DUnitX TestCases project
    Add a Delphi test project for DUnitX: TestCases.dpr implements a console test runner with FastMM4 memory-leak monitoring, optional TestInsight integration, console and NUnit XML loggers, and CI-aware behavior. Include project file TestCases.dproj with build/config settings and a reference to Unit1.pas (new test unit).

---
+++ TestCase/Unit1.pas
@@ -0,0 +1,19 @@
+unit Unit1;
+
+interface
+
+uses
+  DUnitX.TestFramework;
+
+type
+  [TestFixture]
+  TMyTestObject = class
+  public
+  end;
+
+implementation
+
+initialization
+  TDUnitX.RegisterTestFixture(TMyTestObject);
+
+end.
