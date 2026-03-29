1393f9dcb6cfd98174500aa78b43e6067ecafc8f
Diego Farisato <diego.farisato@gmail.com>
2026-03-29 13:25:54 -0300

    Remove manual test runner and adjust dcu output
    Remove the long manual test runner / logging block from TestCases.dpr, leaving only RegisterTestFixture (and the TestInsight path). This simplifies test startup by relying on TestInsight or external runners instead of constructing runner/logger/execute logic inline. In the project file, change DCC_DcuOutput to place DCU files under .\$(Platform)\dcu and remove the Win32 Debug DeployFile entry for TestCases.exe.

--- TestCase/TestCases.dpr
+++ TestCase/TestCases.dpr
@@ -29,60 +29,10 @@ uses
   Cases in 'Cases.pas',
   TestServices in 'TestServices.pas';
 
-{ keep comment here to protect the following conditional from being removed by the IDE when adding a unit }
-{$IFNDEF TESTINSIGHT}
-var
-  runner: ITestRunner;
-  results: IRunResults;
-  logger: ITestLogger;
-  nunitLogger : ITestLogger;
-{$ENDIF}
 begin
   { Register Test Fixture
    *** all future tests must be register here
   }
   RegisterTestFixture;
 
-{$IFDEF TESTINSIGHT}
-  TestInsight.DUnitX.RunRegisteredTests;
-{$ELSE}
-  try
-    //Check command line options, will exit if invalid
-    TDUnitX.CheckCommandLine;
-    //Create the test runner
-    runner := TDUnitX.CreateRunner;
-    //Tell the runner to use RTTI to find Fixtures
-    runner.UseRTTI := True;
-    //When true, Assertions must be made during tests;
-    runner.FailsOnNoAsserts := False;
-
-    //tell the runner how we will log things
-    //Log to the console window if desired
-    if TDUnitX.Options.ConsoleMode <> TDunitXConsoleMode.Off then
-    begin
-      logger := TDUnitXConsoleLogger.Create(TDUnitX.Options.ConsoleMode = TDunitXConsoleMode.Quiet);
-      runner.AddLogger(logger);
-    end;
-    //Generate an NUnit compatible XML File
-    nunitLogger := TDUnitXXMLNUnitFileLogger.Create(TDUnitX.Options.XMLOutputFile);
-    runner.AddLogger(nunitLogger);
-
-    //Run tests
-    results := runner.Execute;
-    if not results.AllPassed then
-      System.ExitCode := EXIT_ERRORS;
-
-    {$IFNDEF CI}
-    //We don't want this happening when running under CI.
-    if TDUnitX.Options.ExitBehavior = TDUnitXExitBehavior.Pause then
-    begin
-      System.Write('Done.. press <Enter> key to quit.');
-      System.Readln;
-    end;
-    {$ENDIF}
-  except
-    on E: Exception do
-      System.Writeln(E.ClassName, ': ', E.Message);
-  end;
-{$ENDIF}
 end.
