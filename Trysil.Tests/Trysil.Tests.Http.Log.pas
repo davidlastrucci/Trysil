de0b71802f49be13df2fdbd38178b33c7d069cc6
David Lastrucci <david.lastrucci@gmail.com>
2026-08-28 10:15:10 +0200

    test(http): cover the log queue cap and the content length gate

--- Trysil.Tests/Trysil.Tests.Http.Log.pas
+++ Trysil.Tests/Trysil.Tests.Http.Log.pas
@@ -17,7 +17,8 @@ uses
   System.JSON,
   DUnitX.TestFramework,
 
-  Trysil.Http.Log.Types;
+  Trysil.Http.Log.Types,
+  Trysil.Http.Log.Classes;
 
 type
 
@@ -31,6 +32,27 @@ type
 
     [Test]
     procedure LogActionPreservesTaskIdAndAction;
+
+    [Test]
+    procedure LogParametersUnlimitedContentAcceptsAnyLength;
+
+    [Test]
+    procedure LogParametersRejectsContentAboveTheCap;
+
+    [Test]
+    procedure LogQueueIsEmptyAfterCreate;
+
+    [Test]
+    procedure LogQueueAcceptsUpToCapacity;
+
+    [Test]
+    procedure LogQueueDiscardsAboveCapacityAndCountsThem;
+
+    [Test]
+    procedure LogQueueTakeDiscardedResetsTheCounter;
+
+    [Test]
+    procedure LogQueueWithNegativeCapacityIsUnbounded;
   end;
 
 implementation
@@ -69,6 +91,145 @@ begin
   LAction := TTHttpLogAction.Create('task-002', 'ProcessRequest');
   Assert.AreEqual('task-002', LAction.TaskID);
   Assert.AreEqual('ProcessRequest', LAction.Action);
+end;
+
+procedure TTHttpLogTests.LogParametersUnlimitedContentAcceptsAnyLength;
+var
+  LParameters: TTHttpLogParameters;
+begin
+  LParameters := TTHttpLogParameters.Create(1, 100);
+  Assert.IsTrue(LParameters.CanLogContent(0));
+  Assert.IsTrue(
+    LParameters.CanLogContent(1024 * 1024),
+    'The two argument constructor must leave the content unlimited');
+
+  LParameters := TTHttpLogParameters.Create(1, 100, -1);
+  Assert.IsTrue(
+    LParameters.CanLogContent(1024 * 1024),
+    'A negative cap must mean unlimited');
+end;
+
+procedure TTHttpLogTests.LogParametersRejectsContentAboveTheCap;
+var
+  LParameters: TTHttpLogParameters;
+begin
+  LParameters := TTHttpLogParameters.Create(1, 100, 100);
+  Assert.IsTrue(LParameters.CanLogContent(99));
+  Assert.IsTrue(
+    LParameters.CanLogContent(100),
+    'A body exactly at the cap must still be logged');
+  Assert.IsFalse(
+    LParameters.CanLogContent(101),
+    'A body above the cap must be omitted');
+end;
+
+procedure TTHttpLogTests.LogQueueIsEmptyAfterCreate;
+var
+  LQueue: TTHttpLogQueue;
+begin
+  LQueue := TTHttpLogQueue.Create(10);
+  try
+    Assert.IsTrue(LQueue.IsEmpty);
+    Assert.AreEqual<Integer>(0, LQueue.TakeDiscarded);
+  finally
+    LQueue.Free;
+  end;
+end;
+
+procedure TTHttpLogTests.LogQueueAcceptsUpToCapacity;
+var
+  LQueue: TTHttpLogQueue;
+  LRequest: TTHttpLogRequest;
+  LValue: TTHttpLogQueueValue;
+begin
+  LRequest := Default(TTHttpLogRequest);
+  LQueue := TTHttpLogQueue.Create(2);
+  try
+    LQueue.Enqueue(LRequest);
+    LQueue.Enqueue(LRequest);
+    Assert.IsFalse(LQueue.IsEmpty);
+    Assert.AreEqual<Integer>(0, LQueue.TakeDiscarded);
+
+    LValue := LQueue.Dequeue;
+    Assert.IsTrue(LValue.QueueType = TTHttpLogQueueType.Request);
+    LQueue.Dequeue;
+    Assert.IsTrue(LQueue.IsEmpty);
+  finally
+    LQueue.Free;
+  end;
+end;
+
+procedure TTHttpLogTests.LogQueueDiscardsAboveCapacityAndCountsThem;
+var
+  LQueue: TTHttpLogQueue;
+  LRequest: TTHttpLogRequest;
+  LResponse: TTHttpLogResponse;
+begin
+  LRequest := Default(TTHttpLogRequest);
+  LResponse := Default(TTHttpLogResponse);
+  LQueue := TTHttpLogQueue.Create(2);
+  try
+    LQueue.Enqueue(LRequest);
+    LQueue.Enqueue(LRequest);
+    LQueue.Enqueue(LRequest);
+    LQueue.Enqueue(LResponse);
+    Assert.AreEqual<Integer>(
+      2,
+      LQueue.TakeDiscarded,
+      'Entries above the capacity must be discarded and counted');
+
+    LQueue.Dequeue;
+    LQueue.Dequeue;
+    Assert.IsTrue(
+      LQueue.IsEmpty,
+      'The queue must never hold more than its capacity');
+  finally
+    LQueue.Free;
+  end;
+end;
+
+procedure TTHttpLogTests.LogQueueTakeDiscardedResetsTheCounter;
+var
+  LQueue: TTHttpLogQueue;
+  LRequest: TTHttpLogRequest;
+begin
+  LRequest := Default(TTHttpLogRequest);
+  LQueue := TTHttpLogQueue.Create(1);
+  try
+    LQueue.Enqueue(LRequest);
+    LQueue.Enqueue(LRequest);
+    Assert.AreEqual<Integer>(1, LQueue.TakeDiscarded);
+    Assert.AreEqual<Integer>(
+      0,
+      LQueue.TakeDiscarded,
+      'Taking the discarded count must reset it');
+  finally
+    LQueue.Free;
+  end;
+end;
+
+procedure TTHttpLogTests.LogQueueWithNegativeCapacityIsUnbounded;
+var
+  LQueue: TTHttpLogQueue;
+  LRequest: TTHttpLogRequest;
+  LIndex: Integer;
+begin
+  LRequest := Default(TTHttpLogRequest);
+  LQueue := TTHttpLogQueue.Create(-1);
+  try
+    for LIndex := 1 to 100 do
+      LQueue.Enqueue(LRequest);
+    Assert.AreEqual<Integer>(
+      0,
+      LQueue.TakeDiscarded,
+      'A negative capacity must mean no limit');
+
+    for LIndex := 1 to 100 do
+      LQueue.Dequeue;
+    Assert.IsTrue(LQueue.IsEmpty);
+  finally
+    LQueue.Free;
+  end;
 end;
 
 initialization
